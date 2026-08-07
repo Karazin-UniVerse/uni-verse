8dbda57e42176d05c775ab101c104776af27d833
LEVKOVSKYI Volodymyr <132926053+Skyzary@users.noreply.github.com>
2026-08-07 14:53:13 +0200

    Update packages/uni-hub/src/app/page.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- packages/uni-hub/src/app/page.tsx
+++ packages/uni-hub/src/app/page.tsx
@@ -13,15 +13,15 @@ export default function Home() {
     <div className={'mx-auto'}>
       <h1 className={'text-2xl font-bold text-center'}>Home</h1>
       <SimpleForm className={'mx-auto mt-16 '}>
-        <label htmlFor="text" className={'flex flex-col items-center gap-2'}>
+        <label className={'flex flex-col items-center gap-2'}>
           <span>Text</span>
           <TextInput placeholder="Enter text" />
         </label>{' '}
-        <label htmlFor="checkbox" className={'flex items-center gap-2'}>
+        <label className={'flex items-center gap-2'}>
           <span>Checkbox</span>
           <CheckBox variant={'primary'} />
         </label>
-        <label htmlFor="" className={'flex flex-col items-center gap-2'}>
+        <label className={'flex flex-col items-center gap-2'}>
           <span>DateTime</span>
           <SimpleDateTime />
         </label>
@@ -29,7 +29,7 @@ export default function Home() {
           <span>Radio</span>
           <RadioButton variant={'primary'} />
         </label>
-        <label htmlFor="" className={'flex flex-col items-center gap-2'}>
+        <label className={'flex flex-col items-center gap-2'}>
           <span>File</span>
           <FileInput />
         </label>{' '}
