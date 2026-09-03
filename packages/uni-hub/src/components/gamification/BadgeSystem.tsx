5ba8b34492b4620b18a562f2da1e5134e985c468
RiderTr <ridertrimer56@gmail.com>
2026-09-03 16:42:43 +0300

    chore(uni-hub): add @una/* path aliases and adopt in gamification components

--- packages/uni-hub/src/components/gamification/BadgeSystem.tsx
+++ packages/uni-hub/src/components/gamification/BadgeSystem.tsx
@@ -3,7 +3,7 @@ import type { Grade } from '@uni-hub/types';
 import { BADGES } from '@uni-hub/constants/gamification';
 import { evaluateBadgeUnlocks } from '@uni-hub/utils/gamification';
 import { useGamificationStore } from '@uni-hub/store/useGamificationStore';
-import { useToast } from '@uni-hub/components/ui/Toast';
+import { useToast } from '@una/Toast';
 
 type BadgeSystemProps = {
   grades: Grade[];
