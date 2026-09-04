da14490a2183ceb0269af890597b779e623a70a0
RiderTr <ridertrimer56@gmail.com>
2026-09-04 18:13:07 +0300

    fix(ui, uni-hub): fix SCSS import paths and migrate to sass:map

--- packages/uni-hub/src/app/layout.tsx
+++ packages/uni-hub/src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import '@uni-hub/index.css';
-import '@uni-hub/design-system/vars.scss';
+import '../../../ui/vars.scss';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
